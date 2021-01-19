b274e4a06eb682fd990283853b5ab1fe516a0011
jennlew <nlewedum@live.com>
2021-01-19 22:00:13 +0000

    starting code for day 12 project

---
+++ day12GuessingGame.py
@@ -0,0 +1,7 @@
+from art import logo
+
+print(logo)
+
+print('Welcome to the number guessing game!')
+print('I\'m thinking of a number between 1 and 100.')
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
