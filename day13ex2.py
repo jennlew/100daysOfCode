9cb53db8ffd60b5d348d95d1de56769e48c85300
jennlew <nlewedum@live.com>
2021-01-23 21:07:05 +0000

    addition of day13 exercise 3 and modification to day13 exercise 1 and 2

--- day13ex2.py
+++ day13ex2.py
@@ -1,3 +1,17 @@
+# year = input("Which year do you want to check?")
+#
+# if year % 4 == 0:
+#   if year % 100 == 0:
+#     if year % 400 == 0:
+#       print("Leap year.")
+#     else:
+#       print("Not leap year.")
+#   else:
+#     print("Leap year.")
+# else:
+#   print("Not leap year.")
+
+# debugged
 year = int(input("Which year do you want to check? "))
 
 if year % 4 == 0:
@@ -10,4 +24,3 @@ if year % 4 == 0:
     print("Leap year.")
 else:
   print("Not leap year.")
-  
