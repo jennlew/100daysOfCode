7a887bc7efab6ce5dde27c425e48e9f9efbcb5f9
jennlew <nlewedum@live.com>
2021-01-24 21:35:29 +0000

    day 14 starting code and todos, game data and logo update

---
+++ day14HigherLower.py
@@ -0,0 +1,15 @@
+# players must guess which person has the most instagram followers
+# if they guess right they move on to the next person and their score is counted (person b from the previous round becomes person a in the next round), if they guess wrong the game ends and their score is printed
+# the game continues until they guess wrong
+
+# todo: import art and game data
+from art import hllogo, vs
+from game_data import data
+
+# todo: welcome player to game and brief intro
+print(logo)
+# todo: display random a and b info and ask user whether a or b has more followers
+# todo: check whether the user has the right answer
+# todo: if the user has the right answer add a point to their total
+# todo: if the user has the right answer make person b person a for the next round
+# todo: if the user has the wrong answer end the game and show them their total score
