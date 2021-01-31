9ea21e6d9cb798878381b13cdf7ad361d7efc830
jennlew <nlewedum@live.com>
2021-01-31 23:03:23 +0000

    day 14 code update

--- day14HigherLower.py
+++ day14HigherLower.py
@@ -18,6 +18,10 @@ def format_data(account):
   accountCountry =account["country"]
   return(f'{accountName}, a {accountDesc} from {accountCountry}')
 
+def check_answer(choice, aFollowerCount, bFollowerCount):
+  """Use if statement to check if answer is correct"""
+
+
 # todo: display random a and b info and ask user whether a or b has more followers
 #generate random account from the game data
 accountA = random.choice(data)
@@ -31,6 +35,8 @@ print(f"Against B: {format_data(accountB)}")
 
 choice = input("Who has more followers? Type 'A' or 'B': ").lower()
 
+aFollowerCount = accountA["follower_count"]
+bFollowerCount = accountB["follower_count"]
 # todo: check whether the user has the right answer
 # make sure that you get the index of the dictionary item that is the number of followers
 #
