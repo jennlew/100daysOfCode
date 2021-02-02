318708cf073875ba78e79bc937202cfc85d48774
jennlew <nlewedum@live.com>
2021-02-02 21:26:04 +0000

    day 14 code complete

--- day14HigherLower.py
+++ day14HigherLower.py
@@ -8,48 +8,51 @@ from game_data import data
 import random
 
 # todo: welcome player to game and brief intro
-print(logo)
+print(hllogo)
 print("Welcome to the higher lower game! Guess which instagram acoount has more followers!")
 
-def format_data(account):
-  """format the account data"""
-  accountName = account["name"]
-  accountDesc = account["description"]
-  accountCountry =account["country"]
-  return(f'{accountName}, a {accountDesc} from {accountCountry}')
-
-def check_answer(choice, aFollowerCount, bFollowerCount):
-  """Use if statement to check if answer is correct"""
-
-
-# todo: display random a and b info and ask user whether a or b has more followers
-#generate random account from the game data
-accountA = random.choice(data)
+score = 0
+gameContinues = True
 accountB = random.choice(data)
-if accountA == accountB:
+
+while gameContinues:
+  def format_data(account):
+    """format the account data"""
+    accountName = account["name"]
+    accountDesc = account["description"]
+    accountCountry =account["country"]
+    return(f'{accountName}, a {accountDesc} from {accountCountry}')
+
+  def check_answer(choice, aFollowers, bFollowers):
+    """Use if statement to check if answer is correct"""
+    if aFollowers > bFollowers:
+      return choice == 'a'
+    else:
+      return choice == 'b'
+
+
+  #display random a and b info and ask user whether a or b has more followers
+  #generate random account from the game data
+  accountA = accountB
   accountB = random.choice(data)
+  while accountA == accountB:
+    accountB = random.choice(data)
 
-print(f"Compare A: {format_data(accountA)}")
-print(vs)
-print(f"Against B: {format_data(accountB)}")
+  print(f"Compare A: {format_data(accountA)}")
+  print(vs)
+  print(f"Against B: {format_data(accountB)}")
 
-choice = input("Who has more followers? Type 'A' or 'B': ").lower()
+  choice = input("Who has more followers? Type 'A' or 'B': ").lower()
 
-aFollowerCount = accountA["follower_count"]
-bFollowerCount = accountB["follower_count"]
-# todo: check whether the user has the right answer
-# make sure that you get the index of the dictionary item that is the number of followers
-#
-  #if A[i] > B[i] and choice == "A"
-    #keep A to move on to the next round
-    #choose new opponent
-    #repeat
-  #elif B[i] > A[i] and choice == "B"
-    #turn B into A for next round
-    #choose new opponent
-    #repeat
-  #else
-    #end game
-# todo: if the user has the right answer add a point to their total
-# todo: if the user has the right answer make person b person a for the next round
-# todo: if the user has the wrong answer end the game and show them their total score
+  aFollowers = accountA["follower_count"]
+  bFollowers = accountB["follower_count"]
+  #check whether the user has the right answer
+  isCorrect = check_answer(choice, aFollowers, bFollowers)
+
+  #give the user feedback
+  if isCorrect:
+    score += 1
+    print(f"You're right! Current score: {score}")
+  else:
+    gameContinues = False
+    print(f"Sorry, that's wrong. Final score: {score}")
