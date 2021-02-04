a45d720cb218126f2c90c2c75ed92bfacc90c7bd
jennlew <nlewedum@live.com>
2021-02-04 23:03:58 +0000

    updating day 15 code

--- day15Coffee.py
+++ day15Coffee.py
@@ -33,8 +33,27 @@ coffeeServed = True
 
 
 while coffeeServed:
-    coffeeChoice = input("What would you like? (espresso/latte/cappuccino)").lower()
+    coffeeChoice = input("What would you like? (espresso/latte/cappuccino) ").lower()
     if coffeeChoice == 'off':
         coffeeServed = False
     if coffeeChoice == 'report':
         print(resources)
+    if coffeeChoice == 'espresso':
+        if resources["water"] < 50:
+            print("Sorry there is not enough water")
+        elif resources["coffee"] < 18:
+            print("Sorry they is not enough coffee")
+    if coffeeChoice == 'latte':
+        if resources["water"] < 200:
+            print("Sorry there is not enough water")
+        elif resources["coffee"] < 24:
+            print("Sorry they is not enough coffee")
+        elif resources["milk"] < 150:
+            print("Sorry there is not enough milk")
+    if coffeeChoice == 'cappuccino':
+        if resources["water"] < 250:
+            print("Sorry there is not enough water")
+        elif resources["coffee"] < 24:
+            print("Sorry they is not enough coffee")
+        elif resources["milk"] < 100:
+            print("Sorry there is not enough milk")
