01031526df0c188509a82f22018cd16f81d3960a
jennlew <nlewedum@live.com>
2021-02-08 20:22:46 +0000

    day 16 code

---
+++ day-16/main.py
@@ -0,0 +1,13 @@
+# import turtle
+#
+# timmy = turtle.Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("DeepPink", "DarkSeaGreen1")
+# timmy.forward(100)
+#
+# myScreen = turtle.Screen()
+# print(myScreen.canvheight)
+# myScreen.exitonclick()
+
+import prettytable
