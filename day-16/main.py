129d5a022077e5d77fe39931412018b6728a09db
jennlew <nlewedum@live.com>
2021-02-09 22:00:01 +0000

    day 16 coffee machine oop code

--- day-16/main.py
+++ day-16/main.py
@@ -10,4 +10,9 @@
 # print(myScreen.canvheight)
 # myScreen.exitonclick()
 
-import prettytable
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = 'l'
+print(table)
