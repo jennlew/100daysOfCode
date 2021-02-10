0d7f81e0c7e41bd5af05154de59cea92959b2f34
jennlew <nlewedum@live.com>
2021-02-10 23:09:41 +0000

    day 16 coffee machine oop code

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -1,3 +1,24 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+moneyMachine = MoneyMachine()
+coffeeMaker = CoffeeMaker()
+menu = Menu()
+makingCoffee = True
+
+
+while makingCoffee:
+    options = menu.get_items()
+    choice = input(f'What would you like? {options}').lower()
+    if choice == 'off':
+        makingCoffee = False
+    elif choice == 'report':
+        coffeeMaker.report()
+        moneyMachine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffeeMaker.is_resource_sufficient(drink):
+            if moneyMachine.make_payment(drink.cost):
+                coffeeMaker.make_coffee(drink)
+
