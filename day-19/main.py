1a351004e4384593e7cccecf8ecf4b892c6e9a19
jennlew <nlewedum@live.com>
2021-02-20 00:10:04 +0000

    day 19

--- day-19/main.py
+++ day-19/main.py
@@ -8,6 +8,30 @@ def move_forwards():
     tim.forward(10)
 
 
+def move_backwards():
+    tim.backward(10)
+
+
+def counter_clockwise():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+
+def clockwise():
+    tim.right(10)
+
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(move_forwards, 'w')
+screen.onkey(move_backwards, 's')
+screen.onkey(clockwise, 'd')
+screen.onkey(counter_clockwise, 'a')
+screen.onkey(clear, 'c')
 screen.exitonclick()
