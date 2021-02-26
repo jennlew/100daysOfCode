c5605207e379848b5fca21d16046c33bee19e024
jennlew <nlewedum@live.com>
2021-02-26 19:11:14 +0000

    day 19 end

--- day-19/main.py
+++ day-19/main.py
@@ -21,8 +21,16 @@ if user_bet:
 
 while is_race_on:
     for turtle in all_turtles:
-        rand_distance = random.randint(0, 10)
-        turtle.forward(rand_distance)
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_colour = turtle.pencolor()
+            if winning_colour == user_bet:
+                print(f"You've won! The {winning_colour} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_colour} turtle is the winner!")
+
+            rand_distance = random.randint(0, 10)
+            turtle.forward(rand_distance)
 # def move_forwards():
 #     tim.forward(10)
 #
