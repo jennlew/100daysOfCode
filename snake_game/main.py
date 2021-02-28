603670d1e2f182bf62fbfa9e75d8e8579a1fb9bc
jennlew <nlewedum@live.com>
2021-02-28 22:50:44 +0000

    snake game start

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from snake import Snake
 import time
 
 screen = Screen()
@@ -7,21 +8,17 @@ screen.bgcolor("black")
 screen.title("Snake")
 screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+snake = Snake()
 
-turtles = []
 
-for position in starting_positions:
-    new_turtle = Turtle("square")
-    new_turtle.color("white")
-    new_turtle.penup()
-    new_turtle.goto(position)
-    turtles.append(new_turtle)
+
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
+
+
     for turt_num in range(len(turtles) - 1, 0, -1):
         new_x = turtles[turt_num - 1].xcor()
         new_y = turtles[turt_num - 1].ycor()
