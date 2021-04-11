2a46552b4f7dfc6f1590360e025e3087f731e4ba
jennlew <nlewedum@live.com>
2021-04-11 18:15:11 +0100

    snake game food functionality and scoreboard added

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,6 +1,10 @@
 from turtle import Screen, Turtle
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
+
+from snake_game import food
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +13,8 @@ screen.title("Snake")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -20,8 +26,12 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    if snake.head.distance(food) < 12:
+        food.refresh()
+        scoreboard.increase_score()
+
 
 
 screen.exitonclick()
