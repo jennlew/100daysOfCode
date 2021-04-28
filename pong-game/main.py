6f96dca358195cfaacf1320a6bfa82ed85538324
jennlew <nlewedum@live.com>
2021-04-28 20:26:20 +0100

    added ball to ponng game

--- pong-game/main.py
+++ pong-game/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
-
+from ball import Ball
+import time
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=600, height=600)
@@ -9,7 +10,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
-
+ball = Ball()
 
 
 screen.listen()
@@ -20,6 +21,8 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 screen.exitonclick()
