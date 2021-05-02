9cdef4e9c54d4665192b3bb7b21bbe86bbafac67
jennlew <nlewedum@live.com>
2021-05-02 11:57:34 +0100

    speed increase and game stop functionality

--- turtle-crossing-start/main.py
+++ turtle-crossing-start/main.py
@@ -18,5 +18,18 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
     car_manager.create_car()
     car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(turtle) < 20:
+            game_is_on = False
+
+    if turtle.is_at_finish_line():
+        turtle.go_to_start()
+        car_manager.level_up()
+
+
+screen.exitonclick()
+
--- turtle-crossing-start/player.py
+++ turtle-crossing-start/player.py
@@ -8,9 +8,18 @@ class Player(Turtle):
     def __init__(self):
         super().__init__()
         self.penup()
-        self.goto(STARTING_POSITION)
+        self.go_to_start()
         self.shape("turtle")
         self.setheading(90)
 
     def go_up(self):
         self.forward(MOVE_DISTANCE)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
