1d8276135d3b1922d8baec3eafe2cd1dc26f26be
jennlew <nlewedum@live.com>
2022-01-27 22:19:42 +0000

    flashcard english translation added

--- flash-card-project-start/main.py
+++ flash-card-project-start/main.py
@@ -8,11 +8,15 @@ data = pandas.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
 current_card = {}
 
+
 def next_card():
-    global current_card
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
 
 
 def flip_card():
@@ -20,11 +24,12 @@ def flip_card():
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
 
+
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-window.after(3000, func=flip_card)
+flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
