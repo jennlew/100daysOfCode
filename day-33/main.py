8924345a6545ffbcceac69ab232e41badd6b1b32
jennlew <nlewedum@live.com>
2022-03-17 20:59:55 +0000

    api iss notifier challenge

--- day-33/main.py
+++ day-33/main.py
@@ -4,9 +4,11 @@ import datetime
 MY_LAT = 51.507351
 MY_LNG = -0.127758
 
+
 parameters = {
     "lat": MY_LAT,
-    "lng": MY_LNG
+    "lng": MY_LNG,
+    "formatted": 0
 }
 response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
@@ -17,5 +19,6 @@ print(data)
 sunrise = data["results"]["sunrise"]
 sunset = data["results"]["sunset"]
 
-time_now = datetime.now()
-
+time_now = datetime.datetime.now()
+print(sunrise.split("T"))
+print(sunset)
