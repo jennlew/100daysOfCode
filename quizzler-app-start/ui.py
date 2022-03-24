d3a619e802f95c2e38f92f80671ea1c61afe78b5
jennlew <nlewedum@live.com>
2022-03-24 21:19:59 +0000

    api trivia quiz challenge

--- quizzler-app-start/ui.py
+++ quizzler-app-start/ui.py
@@ -1,2 +1,13 @@
+from tkinter import  *
 THEME_COLOR = "#375362"
+
+
+class QuizUi:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+
+        self.window.mainloop()
+
 
